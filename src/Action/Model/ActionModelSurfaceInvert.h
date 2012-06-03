a53f304275ac9738fb04e0a3fa6553e8da7ee26a
Michael Ankele <michi@lupina.de>
2012-06-04 00:40:48 +0200

    ActionModelSurfaceInvert added

---
+++ src/Action/Model/ActionModelSurfaceInvert.h
@@ -0,0 +1,28 @@
+/*
+ * ActionModelSurfaceInvert.h
+ *
+ *  Created on: 03.06.2012
+ *      Author: michi
+ */
+
+#ifndef ACTIONMODELSURFACEINVERT_H_
+#define ACTIONMODELSURFACEINVERT_H_
+
+#include "../Action.h"
+#include "../../Data/Data.h"
+
+class ActionModelSurfaceInvert : public Action
+{
+public:
+	ActionModelSurfaceInvert(int _surface);
+	virtual ~ActionModelSurfaceInvert();
+
+	void *execute(Data *d);
+	void undo(Data *d);
+	void redo(Data *d);
+
+private:
+	int surface;
+};
+
+#endif /* ACTIONMODELSURFACEINVERT_H_ */
