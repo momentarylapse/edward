f94ea2ead9b2753a7fa4562188e6f30ef9663c30
Michael Ankele <michi@lupina.de>
2012-06-04 00:41:22 +0200

    ActionModelSurfaceCopy added

---
+++ src/Action/Model/ActionModelSurfaceCopy.h
@@ -0,0 +1,25 @@
+/*
+ * ActionModelSurfaceCopy.h
+ *
+ *  Created on: 03.06.2012
+ *      Author: michi
+ */
+
+#ifndef ACTIONMODELSURFACECOPY_H_
+#define ACTIONMODELSURFACECOPY_H_
+
+#include "../Action.h"
+#include "../ActionGroup.h"
+#include "../../lib/types/types.h"
+#include "../../Data/Model/DataModel.h"
+
+class ActionModelSurfaceCopy : public ActionGroup
+{
+public:
+	ActionModelSurfaceCopy(DataModel *m, ModeModelSurface *&s);
+	virtual ~ActionModelSurfaceCopy();
+
+	void *execute_return(Data *d);
+};
+
+#endif /* ACTIONMODELSURFACECOPY_H_ */
