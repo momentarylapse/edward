b9a0e545dbff9f5c9f2dcc0419d4265b368f323c
Michael Ankele <michi@lupina.de>
2012-06-12 14:01:02 +0200

    TerrainPropertiesDialog added as dummy

---
+++ src/Mode/World/Dialog/TerrainPropertiesDialog.cpp
@@ -0,0 +1,19 @@
+/*
+ * TerrainPropertiesDialog.cpp
+ *
+ *  Created on: 12.06.2012
+ *      Author: michi
+ */
+
+#include "TerrainPropertiesDialog.h"
+
+TerrainPropertiesDialog::TerrainPropertiesDialog()
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+TerrainPropertiesDialog::~TerrainPropertiesDialog()
+{
+	// TODO Auto-generated destructor stub
+}
---
+++ src/Mode/World/Dialog/TerrainPropertiesDialog.h
@@ -0,0 +1,18 @@
+/*
+ * TerrainPropertiesDialog.h
+ *
+ *  Created on: 12.06.2012
+ *      Author: michi
+ */
+
+#ifndef TERRAINPROPERTIESDIALOG_H_
+#define TERRAINPROPERTIESDIALOG_H_
+
+class TerrainPropertiesDialog
+{
+public:
+	TerrainPropertiesDialog();
+	virtual ~TerrainPropertiesDialog();
+};
+
+#endif /* TERRAINPROPERTIESDIALOG_H_ */
