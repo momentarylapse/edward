e2c878d6c8ddf4cf522bb09a95106d444709aa73
Michael Ankele <michi@lupina.de>
2012-06-15 22:42:27 +0200

    ActionWorldAddTerrain: allowing load by filename

--- src/Action/World/ActionWorldAddTerrain.cpp
+++ src/Action/World/ActionWorldAddTerrain.cpp
@@ -9,6 +9,13 @@
 #include "../../Data/World/DataWorld.h"
 #include <assert.h>
 
+ActionWorldAddTerrain::ActionWorldAddTerrain(const vector &_pos, const string &_filename)
+{
+	filename = _filename;
+	assert(filename.num > 0);
+	pos = _pos;
+}
+
 ActionWorldAddTerrain::ActionWorldAddTerrain(const vector &_pos, const vector &_size, int _num_x, int _num_z)
 {
 	pos = _pos;
@@ -26,6 +33,7 @@ void ActionWorldAddTerrain::undo(Data *d)
 	DataWorld *w = dynamic_cast<DataWorld*>(d);
 	assert(w->Terrain.num > 0);
 
+	delete(w->Terrain.back().terrain);
 	w->Terrain.pop();
 }
 
@@ -41,32 +49,36 @@ void *ActionWorldAddTerrain::execute(Data *d)
 	t.is_selected = true;
 	t.is_special = false;
 	t.pos = pos;
-	t.terrain = new CTerrain();
-	t.terrain->pos = pos;
-	t.terrain->num_x = num_x;
-	t.terrain->num_z = num_z;
-	int num = (num_x + 1) * (num_z + 1);
-	t.terrain->height.resize(num);
-	t.terrain->normal.resize(num);
-	t.terrain->vertex.resize(num);
-	t.terrain->pattern.x = size.x / num_x;
-	t.terrain->pattern.y = 0;
-	t.terrain->pattern.z = size.z / num_z;
-	t.terrain->num_textures = 1;
-	t.terrain->texture[0] = -1;
-	t.terrain->texture_scale[0].x = 1.0f / num_x;
-	t.terrain->texture_scale[0].y = 0;
-	t.terrain->texture_scale[0].z = 1.0f / num_z;
-	t.terrain->material = MetaLoadMaterial("");
-	for (int x=0;x<num_x/32+1;x++)
-		for (int z=0;z<num_z/32+1;z++)
-			t.terrain->partition[x][z] = -1;
-	t.terrain->vertex_buffer = NixCreateVB(65536);
-	t.terrain->Update(-1, -1, -1, -1, TerrainUpdateAll);
-	// bounding box
-	t.terrain->min = pos;
-	t.terrain->max = pos + size;
-	t.terrain->force_redraw = true;
+
+	if (filename == ""){
+		t.terrain = new CTerrain();
+		t.terrain->pos = pos;
+		t.terrain->num_x = num_x;
+		t.terrain->num_z = num_z;
+		int num = (num_x + 1) * (num_z + 1);
+		t.terrain->height.resize(num);
+		t.terrain->normal.resize(num);
+		t.terrain->vertex.resize(num);
+		t.terrain->pattern.x = size.x / num_x;
+		t.terrain->pattern.y = 0;
+		t.terrain->pattern.z = size.z / num_z;
+		t.terrain->num_textures = 1;
+		t.terrain->texture[0] = -1;
+		t.terrain->texture_scale[0].x = 1.0f / num_x;
+		t.terrain->texture_scale[0].y = 0;
+		t.terrain->texture_scale[0].z = 1.0f / num_z;
+		t.terrain->material = MetaLoadMaterial("");
+		for (int x=0;x<num_x/32+1;x++)
+			for (int z=0;z<num_z/32+1;z++)
+				t.terrain->partition[x][z] = -1;
+		t.terrain->vertex_buffer = NixCreateVB(65536);
+		t.terrain->Update(-1, -1, -1, -1, TerrainUpdateAll);
+		// bounding box
+		t.terrain->min = pos;
+		t.terrain->max = pos + size;
+		t.terrain->force_redraw = true;
+	}else
+		t.terrain = new CTerrain(filename, pos);
 
 	w->Terrain.add(t);
 	return &w->Terrain.back();
--- src/Action/World/ActionWorldAddTerrain.h
+++ src/Action/World/ActionWorldAddTerrain.h
@@ -14,6 +14,7 @@
 class ActionWorldAddTerrain : public Action
 {
 public:
+	ActionWorldAddTerrain(const vector &_pos, const string &_filename);
 	ActionWorldAddTerrain(const vector &_pos, const vector &_size, int _num_x, int _num_z);
 	virtual ~ActionWorldAddTerrain();
 	string name(){	return "WorldAddTerrain";	}
@@ -22,6 +23,7 @@ public:
 	void undo(Data *d);
 
 private:
+	string filename;
 	vector pos;
 	vector size;
 	int num_x, num_z;
