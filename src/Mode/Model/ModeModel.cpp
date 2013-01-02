cab1f232baf74622120fb8e2db71475f0afb702b
Michael Ankele <michi@lupina.de>
2013-01-02 15:07:45 +0100

    remove irritating model tool buttons

--- src/Mode/Model/ModeModel.cpp
+++ src/Mode/Model/ModeModel.cpp
@@ -62,9 +62,9 @@ void ModeModel::OnStart()
 	ed->ToolbarAddItemCheckable(_("Mesh"),_("Mesh"), dir + "model_mesh.svg", "mode_model_mesh");
 	ed->ToolbarAddItemCheckable(_("Skelett"),_("Skelett"), dir + "mode_skeletton.png", "mode_model_skeleton");
 	ed->ToolbarAddItemCheckable(_("Animation"),_("Animation"), dir + "mode_move.png", "mode_model_animation");
-	ed->ToolbarAddItem(_("Texturen"),_("Texturen"), dir + "mode_textures.png", "mode_model_texture");
-	ed->ToolbarAddItem(_("Material"),_("Material"), dir + "mode_textures.png", "mode_model_material");
-	ed->ToolbarAddItem(_("Materialien"),_("Materialien"), dir + "mode_textures.png", "mode_model_materials");
+	//ed->ToolbarAddItem(_("Texturen"),_("Texturen"), dir + "mode_textures.png", "mode_model_texture");
+	ed->ToolbarAddItem(_("Material"),_("aktuelles Material"), dir + "mode_textures.png", "mode_model_material");
+	//ed->ToolbarAddItem(_("Materialien"),_("Materialien"), dir + "mode_textures.png", "mode_model_materials");
 	ed->ToolbarAddItem(_("Eigenschaften"),_("Eigenschaften"), dir + "configure.png", "mode_properties");
 	ed->EnableToolbar(true);
 	ed->ToolbarConfigure(false,true);
