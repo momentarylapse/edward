4681cac73763384da84f3b18193184b6201d96c5
Michael Ankele <michi@lupina.de>
2025-04-23 01:08:17 +0200

    --heightmap

--- src/mode_world/dialog/EditTerrainPanel.cpp
+++ src/mode_world/dialog/EditTerrainPanel.cpp
@@ -7,8 +7,10 @@
 #include "../action/terrain/ActionWorldResizeTerrain.h"
 #include "../data/DataWorld.h"
 #include <lib/base/tuple.h>
+#include <lib/image/image.h>
 #include <lib/xhui/Dialog.h>
 #include <lib/xhui/xhui.h>
+#include <storage/Storage.h>
 #include <world/Terrain.h>
 
 class IntPairDialog : public xhui::Dialog {
@@ -52,6 +54,44 @@ Dialog int-pair-dialog 'Question'
 	}
 };
 
+class FloatDialog : public xhui::Dialog {
+public:
+	FloatDialog(xhui::Panel* parent, const string& question, float f) : Dialog("Question", 320, 120, parent, xhui::DialogFlags::CloseByEscape) {
+		from_source(R"foodelim(
+Dialog int-pair-dialog 'Question'
+	Grid ? ''
+		Label question ''
+		---|
+		SpinButton value '' range=-9999:9999:0.001 expandx
+		---|
+		Grid ? ''
+			Button ok 'Ok'
+			Button cancel 'Cancel'
+)foodelim");
+		set_string("question", question);
+		set_float("value", f);
+		event("ok", [this] {
+			promise(get_float("value"));
+			request_destroy();
+		});
+		event("cancel", [this] {
+			promise.fail();
+			request_destroy();
+		});
+		event(xhui::event_id::Close, [this] {
+			promise.fail();
+			request_destroy();
+		});
+	}
+	base::promise<float> promise;
+
+	static base::future<float> ask(xhui::Panel* parent, const string& question, float f) {
+		auto dlg = new FloatDialog(parent, question, f);
+		parent->open_dialog(dlg);
+		return dlg->promise.get_future();
+	}
+};
+
 EditTerrainPanel::EditTerrainPanel(ModeEditTerrain* mode) : Node<xhui::Panel>("edit-terrain-panel") {
 	mode_terrain = mode;
 	from_source(R"foodelim(
@@ -78,6 +118,19 @@ Dialog edit-terrain-panel ''
 		auto& t = mode_terrain->terrain();
 		IntPairDialog::ask(this, "New terrain size", t.terrain->num_x, t.terrain->num_z).then([this] (const base::tuple<int,int>& size) {
 			mode_terrain->data->execute(new ActionWorldResizeTerrain(mode_terrain->index, size.a, size.b));
+		});
+	});
+	event("load-heightmap", [this] {
+		mode_terrain->session->storage->file_dialog(FD_TEXTURE, false, false).then([this] (const ComplexPath& path) {
+			FloatDialog::ask(this, "Maximum height", 10.0f).then([this, path] (float scale) {
+				auto& t = mode_terrain->terrain();
+				auto im = Image::load(path.complete);
+				for (int i=0; i<t.terrain->num_x; i++)
+					for (int j=0; j<t.terrain->num_z; j++)
+						t.terrain->height[i * (t.terrain->num_z+1) + j] = im->get_pixel(i % im->width, j % im->height).brightness() * scale;
+				t.terrain->update(-1, -1, -1, -1, TerrainUpdateAll);
+				delete im;
+			});
 		});
 	});
 }
