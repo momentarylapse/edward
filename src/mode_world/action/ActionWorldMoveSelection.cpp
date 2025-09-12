32cf6bf2ae5529cb2b9290c01175a38ca50535de
Michael Ankele <michi@lupina.de>
2025-09-12 15:04:46 +0200

    -a

--- src/mode_world/action/ActionWorldMoveSelection.cpp
+++ src/mode_world/action/ActionWorldMoveSelection.cpp
@@ -9,13 +9,12 @@
 #include <lib/base/iter.h>
 #include <lib/math/quaternion.h>
 #include "../data/DataWorld.h"
-#include "../data/WorldCamera.h"
 #include "../data/WorldLight.h"
 #include "../data/WorldLink.h"
-#include "../data/WorldObject.h"
-#include "../data/WorldTerrain.h"
 #include <y/world/Terrain.h>
 #include "../../Session.h"
+#include <y/EntityManager.h>
+#include <y/Entity.h>
 
 ActionWorldMoveSelection::ActionWorldMoveSelection(DataWorld *d, const Data::Selection& selection) {
 	// list of selected objects and save old pos
@@ -35,10 +34,19 @@ ActionWorldMoveSelection::ActionWorldMoveSelection(DataWorld *d, const Data::Sel
 				old_ang.add(quaternion::ID);
 				type.add(MultiViewType::WORLD_LINK);
 			}
+
+	if (selection.contains(MultiViewType::WORLD_ENTITY))
+		for (const auto& [i, o]: enumerate(d->entity_manager->entities))
+			if (selection[MultiViewType::WORLD_ENTITY].contains(i)) {
+				index.add(i);
+				old_data.add(o->pos);
+				old_ang.add(o->ang);
+				type.add(MultiViewType::_NUM); // dummy...
+			}
 }
 
 void *ActionWorldMoveSelection::execute(Data *d) {
-	DataWorld *w = dynamic_cast<DataWorld*>(d);
+	auto w = dynamic_cast<DataWorld*>(d);
 	auto dq = quaternion::rotation(mat);
 	for (const auto& [ii, i]: enumerate(index)) {
 		if (type[ii] == MultiViewType::WORLD_ENTITY) {
@@ -46,19 +54,25 @@ void *ActionWorldMoveSelection::execute(Data *d) {
 			w->entities[i].ang = dq * old_ang[ii];
 		} else if (type[ii] == MultiViewType::WORLD_LINK) {
 			w->links[i].pos = mat * old_data[ii];
+		} else if (type[ii] == MultiViewType::_NUM) {
+			w->entity_manager->entities[i]->pos = mat * old_data[ii];
+			w->entity_manager->entities[i]->ang = dq * old_ang[ii];
 		}
 	}
 	return nullptr;
 }
 
 void ActionWorldMoveSelection::undo(Data *d) {
-	DataWorld *w = dynamic_cast<DataWorld*>(d);
+	auto w = dynamic_cast<DataWorld*>(d);
 	for (const auto& [ii, i]: enumerate(index)) {
 		if (type[ii] == MultiViewType::WORLD_ENTITY) {
 			w->entities[i].pos = old_data[ii];
 			w->entities[i].ang = old_ang[ii];
 		} else if (type[ii] == MultiViewType::WORLD_LINK) {
 			w->links[i].pos = old_data[ii];
+		} else if (type[ii] == MultiViewType::_NUM) {
+			w->entity_manager->entities[i]->pos = old_data[ii];
+			w->entity_manager->entities[i]->ang = old_ang[ii];
 		}
 	}
 }
